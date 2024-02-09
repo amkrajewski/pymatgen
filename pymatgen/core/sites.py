56c1e7d7f2586dae2dabc3a1fcf0ea3c7ed1845b
Adam M. Krajewski <54290107+amkrajewski@users.noreply.github.com>
2024-02-08 20:55:03 -0500

    - (fix) old RST docs for PeriodicSite did not render correctly. Translated them to the Google style.

--- pymatgen/core/sites.py
+++ pymatgen/core/sites.py
@@ -191,9 +191,9 @@ class Site(collections.abc.Hashable, MSONable):
         return self.species[el]
 
     def __eq__(self, other: object) -> bool:
-        """Site is equal to another site if the species and occupancies are the
-        same, and the coordinates are the same to some tolerance.  numpy
-        function `allclose` is used to determine if coordinates are close.
+        """A site is equal to another site if the species and occupancies are the
+        same, and the coordinates are the same to some tolerance. numpy function
+        `allclose` is used to determine if coordinates are close.
         """
         if not isinstance(other, type(self)):
             return NotImplemented
@@ -284,8 +284,8 @@ class Site(collections.abc.Hashable, MSONable):
 
 
 class PeriodicSite(Site, MSONable):
-    """Extension of generic Site object to periodic systems.
-    PeriodicSite includes a lattice system.
+    """Extension of the generic `Site` object to periodic systems.
+    `PeriodicSite` includes a lattice system.
     """
 
     def __init__(
@@ -301,29 +301,32 @@ class PeriodicSite(Site, MSONable):
     ) -> None:
         """Create a periodic site.
 
-        :param species: Species on the site. Can be:
-            i.  A Composition-type object (preferred)
-            ii. An  element / species specified either as a string
-                symbols, e.g. "Li", "Fe2+", "P" or atomic numbers,
-                e.g., 3, 56, or actual Element or Species objects.
-            iii.Dict of elements/species and occupancies, e.g.,
-                {"Fe" : 0.5, "Mn":0.5}. This allows the setup of
-                disordered structures.
-        :param coords: Coordinates of site, fractional coordinates
-            by default. See ``coords_are_cartesian`` for more details.
-        :param lattice: Lattice associated with the site.
-        :param to_unit_cell: Translates fractional coordinate to the
-            basic unit cell, i.e. all fractional coordinates satisfy 0
-            <= a < 1. Defaults to False.
-        :param coords_are_cartesian: Set to True if you are providing
-            Cartesian coordinates. Defaults to False.
-        :param properties: Properties associated with the site as a dict, e.g.
-            {"magmom": 5}. Defaults to None.
-        :param label: Label for the site. Defaults to None.
-        :param skip_checks: Whether to ignore all the usual checks and just
-            create the site. Use this if the PeriodicSite is created in a
-            controlled manner and speed is desired.
+        Args:
+            species (SpeciesLike | CompositionLike): Species on the site.
+                They can be:
+                i.  A Composition-type object (preferred)
+                ii. An element / species specified either as a string
+                    symbols, e.g. "Li", "Fe2+", "P" or atomic numbers,
+                    e.g., 3, 56, or actual Element or Species objects.
+                iii.Dict of elements/species and occupancies, e.g.,
+                    {"Fe" : 0.5, "Mn":0.5}. This allows the setup of
+                    disordered structures.
+            coords (ArrayLike): Coordinates of site, fractional coordinates
+                by default.
+            lattice (Lattice): Lattice associated with the site.
+            to_unit_cell (bool): Translates fractional coordinate to the basic
+                unit cell, i.e. all fractional coordinates satisfy 0 <= a < 1.
+                Defaults to False.
+            coords_are_cartesian (bool): Set to True if you are providing Cartesian
+                coordinates. Defaults to False.
+            properties (dict): Properties associated with the site as a dict, e.g.
+                {"magmom": 5}. Defaults to None.
+            label (str): Label for the site. Defaults to None.
+            skip_checks (bool): Whether to ignore all the usual checks and just create
+                the site. Use this if the PeriodicSite is created in a controlled manner
+                and speed is desired.
         """
+
         frac_coords = lattice.get_fractional_coords(coords) if coords_are_cartesian else coords
 
         if to_unit_cell:
